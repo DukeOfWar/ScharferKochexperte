8f03d584b3050ae244006414da4fd0863441ab0c
DukeOfWar <93122756+DukeOfWar@users.noreply.github.com>
2022-04-30 13:00:23 +0200

    Button "Klick me"

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,23 @@ namespace TestForms
 {
     public partial class Form1 : Form
     {
+        public Button button1;
         public Form1()
         {
             InitializeComponent();
+
+            button1 = new Button();
+            button1.Size = new Size(60, 80);
+            button1.Location = new Point(30, 30);
+            button1.Text = "Click me";
+            this.Controls.Add(button1);
+            button1.Click += new EventHandler(button1_Click);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
         }
 
         //Test
